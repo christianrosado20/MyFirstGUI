ac5c96328a7d4f710ee729d83588e1d0be92bc5a
Christian Rosado <christian.rosado20@upr.edu>
2019-03-20 13:00:10 -0400

    updating

--- MyFirstGUI/src/Main.java
+++ MyFirstGUI/src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		JFrame mainFrame = new JFrame("My First GUI");
 		mainFrame.setSize(800, 600);
@@ -10,5 +10,9 @@ public class Main {
 		mainFrame.add(myComponent);
 		mainFrame.setVisible(true);
 		
+		while(true) {
+			mainFrame.repaint();
+			Thread.sleep(100);
+		}
 	}
 }
--- MyFirstGUI/src/MyComponent.java
+++ MyFirstGUI/src/MyComponent.java
@@ -1,6 +1,10 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.util.Random;
 
 import javax.swing.JComponent;
 
@@ -8,11 +12,23 @@ public class MyComponent extends JComponent{
 
 	private static int counter = 0;
 	
+	private Car topLeftCar = new Car(0,0,Color.RED);
+	private Car bottomRightCar = new Car(0,0);
+
 	public void paintComponent(Graphics g) {
+//		Car topLeftCar = new Car(0,0,Color.red);
+		topLeftCar.draw(g);
 		
-		Graphics2D g2 = (Graphics2D) g;
-		Rectangle body = new Rectangle(10,20,60,10);
-		g2.draw(body);
+//		Car bottomRightCar = new Car(this.getWidth()-60, this.getHeight()-30);
+		bottomRightCar.setxPos(this.getWidth()-60);
+		bottomRightCar.setyPos(this.getHeight()-30);
+		bottomRightCar.draw(g);
+		
+		Random genRandom = new Random();
+		int randomX = genRandom.nextInt(this.getWidth()-60);
+		int randomY = genRandom.nextInt(this.getHeight()-30);
+		Car randomCar = new Car(randomX, randomY, Color.magenta);
+		randomCar.draw(g);
 		
 		counter++;
 		System.out.println("paintComponent called " + counter + " times.");
